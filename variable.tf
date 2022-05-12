a07185a550cbc558fa768549886498ecee25ca75
Rajnish Kumar Singh <84284872+rajnish197@users.noreply.github.com>
2022-05-12 07:44:31 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "Demo-Web"
+  default     =  "terrakey"
 }
 
 variable "instance_type" {
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "sg-0f3d85c96479606b6"
 }
 
 variable "tag_name" {
